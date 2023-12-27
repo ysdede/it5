a3d9dee2bf30f28a5ff8d7ff5a33de0737b87ff5
ysdede <yunusseyhandede@gmail.com>
2023-12-27 13:55:39 +0300

    Fix typo

--- finetuning/run_grammar.py
+++ finetuning/run_grammar.py
@@ -578,7 +578,7 @@ def main():
         logger.info(f"Eval batch avg. Levenshtein distance: {lev_dist_avg}")
         prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
         result["gen_len"] = np.mean(prediction_lens)
-        results["Levenshtein"] = lev_dist_avg
+        result["Levenshtein"] = lev_dist_avg
         result = {k: round(v, 4) for k, v in result.items()}
         return result
 
